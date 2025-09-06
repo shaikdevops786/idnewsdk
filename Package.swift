26c12a5fa92bf7bf635368ad1947528361067f9c
Shaik Amair <shaikamair@MacBookPro.lan>
2025-09-07 00:54:07 +0530

    Use 1.0.1 asset + checksum

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "idnewsdk",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "idnewsdk", targets: ["passportidSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "passportidSDK",
+            url: "https://github.com/shaikdevops786/idnewsdk/releases/download/1.0.1/passportidSDK.xcframework.zip",
+            checksum: "5145f70d8b81fce887654e4da482512139dc7e6f2ca8b081a67bed1b1217a86c"
+        )
+    ]
+)
